4064a93124c9384c20a5baf98594ea23e2660483
JakeWeissman <weissman.jake@gmail.com>
2016-02-03 21:18:32 -0500

    setup front end to make simple connection to backend

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -1,53 +1,10 @@
-angular.module('nodeTodo', [])
+angular.module('sportsApp', [])
 
-.controller('mainController', function($scope, $http) {
+.controller('mainCtrl', function($scope, $http) {
 
-    $scope.formData = {};
-    $scope.todoData = {};
+	$scope.teams = [];
 
-    // Get all todos
-    $http.get('/api/v1/todos')
-        .success(function(data) {
-            $scope.todoData = data;
-            console.log(data);
-        })
-        .error(function(error) {
-            console.log('Error: ' + error);
-    });
-
-    // // Create a new todo
-    // $http.post('/api/v1/todos', $scope.formData)
-    //     .success(function(data) {
-    //         $scope.formData = {};
-    //         $scope.todoData = data;
-    //         console.log(data);
-    //     })
-    //     .error(function(error) {
-    //         console.log('Error: ' + error);
-    //     });
-
-    // // Delete a todo
-    // $scope.deleteTodo = function(todoID) {
-    //     $http.delete('/api/v1/todos/' + todoID)
-    //         .success(function(data) {
-    //             $scope.todoData = data;
-    //             console.log(data);
-    //         })
-    //         .error(function(data) {
-    //             console.log('Error: ' + data);
-    //         });
-    // };
-
-    // // Create a new todo
-    // $scope.createTodo = function(todoID) {
-    //     $http.post('/api/v1/todos', $scope.formData)
-    //         .success(function(data) {
-    //             $scope.formData = {};
-    //             $scope.todoData = data;
-    //             console.log(data);
-    //         })
-    //         .error(function(error) {
-    //             console.log('Error: ' + error);
-    //         });
-    // };
+	$http.get('/all_teams').then(function(response) {
+		$scope.teams = response.data;
+	})
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,24 @@ var router = express.Router();
 var path = require('path');
 var pg = require('pg');
 var connectionString = require(path.join(__dirname, '../', '../', 'config'));
+var client = new pg.Client(connectionString);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', '../', 'client', 'views', 'index.html'));
+});
+
+router.get('/all_teams', function(req, res, next) {
+  client.connect();
+  var results = [];
+  var query = client.query("SELECT * FROM teams ORDER BY name ASC");
+  query.on('row', function(row) {
+    results.push(row);
+  });
+  query.on('end', function() {
+    client.end();
+    return res.json(results);
+  });
 });
 
 router.post('/api/v1/todos', function(req, res) {
