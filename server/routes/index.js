44528bb7888342725015a515b9119c5809d55528
JakeWeissman <weissman.jake@gmail.com>
2016-02-04 10:28:01 -0500

    bug fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,20 +5,20 @@ var pg = require('pg');
 var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 var client = new pg.Client(connectionString);
 
+client.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', '../', 'client', 'views', 'index.html'));
 });
 
 router.get('/all_teams', function(req, res, next) {
-  client.connect();
   var results = [];
   var query = client.query("SELECT * FROM teams ORDER BY name ASC");
   query.on('row', function(row) {
     results.push(row);
   });
   query.on('end', function() {
-    client.end();
     return res.json(results);
   });
 });
