314c869aefa013f81bb1759861aa49d2f178df74
Alex Stein <aas2272>
2016-02-05 13:59:05 -0500

    database

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/nbadb';
+// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/nbadb';
+
+var connectionString = process.env.DATABASE_URL || 'postgres://wsfsuiesjspoxd:MjZ3URxFgwqsXv1GP72Ez8Pv2V@ec2-54-83-59-203.compute-1.amazonaws.com:5432/d3n9d7enm5ut44';
+
 
 module.exports = connectionString;
